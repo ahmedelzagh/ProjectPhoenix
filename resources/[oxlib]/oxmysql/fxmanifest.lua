fc87182fce297fb197c454cc64fc2a1b9e25bf12
Ahmed Elzagh <ahmedelzagh50@gmail.com>
2026-01-06 13:32:14 +0200

    chore: update resources.cfg, upgrade oxmysql, and add libs dependencies

--- resources/[oxlib]/oxmysql/fxmanifest.lua
+++ resources/[oxlib]/oxmysql/fxmanifest.lua
@@ -1,14 +1,17 @@
 fx_version 'cerulean'
 game 'common'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
 
 name 'oxmysql'
-description 'Database wrapper for FXServer utilising node-mysql2 offering improved performance and security.'
-version '2.6.0'
-url 'https://github.com/overextended/oxmysql'
-author 'overextended'
+author 'Overextended'
+version '2.12.3'
+license 'LGPL-3.0-or-later'
+repository 'https://github.com/communityox/oxmysql.git'
+description 'FXServer to MySQL communication via node-mysql2'
 
 dependencies {
-	'/server:5104',
+    '/server:7290',
 }
 
 client_script 'ui.lua'
