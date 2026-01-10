eb100c3a6845c7142cfd4a1737a4600204f916f5
Ahmed Elzagh <ahmedelzagh50@gmail.com>
2026-01-10 06:27:55 +0200

    Rename MRPD resource to community-mrpd and remove GABZ references

--- resources/[gabz]/community-mrpd/fxmanifest.lua
+++ resources/[gabz]/community-mrpd/fxmanifest.lua
@@ -1,21 +1,21 @@
 fx_version 'bodacious'
 game 'gta5'
 
-author 'GABZ'
-description 'MRPD'
+author 'Community'
+description 'Community MRPD'
 version '1.0.0'
 
 this_is_a_map 'yes'
 
 
-data_file 'TIMECYCLEMOD_FILE' 'gabz_mrpd_timecycle.xml'
+data_file 'TIMECYCLEMOD_FILE' 'community_mrpd_timecycle.xml'
 data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
 
 files {
-	'gabz_mrpd_timecycle.xml',
+	'community_mrpd_timecycle.xml',
 	'interiorproxies.meta'
 }
 
 client_script {
-    "gabz_mrpd_entitysets.lua"
+    "community_mrpd_entitysets.lua"
 }
